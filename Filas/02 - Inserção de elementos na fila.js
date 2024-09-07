6e4c91e3bccc40b5fe973ffe2b4a9009879d7d71
Luís André <andre2016999@gmail.com>
2024-09-06 23:05:02 -0300

    Inserção de elementos na fila

---
+++ Filas/02 - Inserção de elementos na fila.js
@@ -0,0 +1,48 @@
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    enqueue(element) {
+        this.items.push(element);
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return 'Queue is empty';
+        }
+        return this.items.shift();
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    front() {
+        if (this.isEmpty()) {
+            return 'Queue is empty';
+        }
+        return this.items[0];
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    clear() {
+        this.items = [];
+    }
+
+    toString() {
+        return this.items.toString();
+    }
+}
+
+const queue = new Queue();
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+
+console.log(queue.toString());
+console.log(queue.size());
