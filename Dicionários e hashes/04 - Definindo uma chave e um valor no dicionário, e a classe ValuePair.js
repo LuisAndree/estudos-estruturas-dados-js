5b31aefb5ed4832969841aa12dd10e132d55c011
Luís André <andre2016999@gmail.com>
2024-11-16 23:26:31 -0300

    Definindo uma chave e um valor no dicionário, e a classe ValuePair

---
+++ Dicionários e hashes/04 - Definindo uma chave e um valor no dicionário, e a classe ValuePair.js
@@ -0,0 +1,69 @@
+// Classe auxiliar para representar um par chave-valor
+class ValuePair {
+    constructor(key, value) {
+      this.key = key;
+      this.value = value;
+    }
+  
+    toString() {
+      return `[#${this.key}: ${this.value}]`;
+    }
+  }
+  
+  // Classe Dictionary
+  class Dictionary {
+    constructor() {
+      this.items = {}; // Armazena pares chave-valor como objetos
+    }
+  
+    // Adiciona ou atualiza uma chave-valor
+    set(key, value) {
+      this.items[key] = new ValuePair(key, value);
+    }
+  
+    // Retorna o valor associado a uma chave
+    get(key) {
+      return this.has(key) ? this.items[key].value : undefined;
+    }
+  
+    // Verifica se uma chave existe
+    has(key) {
+      return Object.prototype.hasOwnProperty.call(this.items, key);
+    }
+  
+    // Remove uma chave e seu valor
+    remove(key) {
+      if (this.has(key)) {
+        delete this.items[key];
+        return true;
+      }
+      return false;
+    }
+  
+    // Retorna todas as chaves
+    keys() {
+      return Object.keys(this.items);
+    }
+  
+    // Retorna todos os valores
+    values() {
+      return Object.values(this.items).map((valuePair) => valuePair.value);
+    }
+  
+    // Retorna todas as entradas (chave-valor como objetos ValuePair)
+    entries() {
+      return Object.values(this.items);
+    }
+  
+    // Retorna o número de pares chave-valor
+    size() {
+      return Object.keys(this.items).length;
+    }
+  
+    // Remove todos os itens do dicionário
+    clear() {
+      this.items = {};
+    }
+  }
+  
+  
