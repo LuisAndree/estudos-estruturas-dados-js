716f417a614a846f17c9729f61bb024ad68161e7
Luís André <andre2016999@gmail.com>
2024-12-06 23:54:59 -0300

    Algoritmo de Kruskal

--- Grafos/17 - Algoritmo de Kruskal.js
+++ Grafos/17 - Algoritmo de Kruskal.js
@@ -7,19 +7,16 @@ class Edge {
     }
   }
   
-  // Classe para representar um grafo
   class Graph {
     constructor(vertices) {
-      this.vertices = vertices; // Número de vértices no grafo
-      this.edges = []; // Lista de arestas
+      this.vertices = vertices; 
+      this.edges = []; 
     }
   
-    // Adiciona uma aresta ao grafo
     addEdge(src, dest, weight) {
       this.edges.push(new Edge(src, dest, weight));
     }
   
-    // Encontra o conjunto de um vértice (comprimindo caminho)
     find(parent, vertex) {
       if (parent[vertex] !== vertex) {
         parent[vertex] = this.find(parent, parent[vertex]);
@@ -27,7 +24,6 @@ class Edge {
       return parent[vertex];
     }
   
-    // Realiza a união de dois subconjuntos
     union(parent, rank, x, y) {
       const xRoot = this.find(parent, x);
       const yRoot = this.find(parent, y);
@@ -42,26 +38,22 @@ class Edge {
       }
     }
   
-    // Algoritmo de Kruskal para encontrar a MST
     kruskalMST() {
-      const result = []; // Armazena as arestas da MST
-      const parent = []; // Array para representar os conjuntos disjuntos
-      const rank = []; // Array para armazenar o rank dos conjuntos
+      const result = []; 
+      const parent = []; 
+      const rank = []; 
   
-      // Inicializa os conjuntos disjuntos
       for (let i = 0; i < this.vertices; i++) {
         parent[i] = i;
         rank[i] = 0;
       }
   
-      // Ordena as arestas pelo peso
       this.edges.sort((a, b) => a.weight - b.weight);
   
       for (const edge of this.edges) {
         const x = this.find(parent, edge.src);
         const y = this.find(parent, edge.dest);
   
-        // Inclui a aresta se ela não formar um ciclo
         if (x !== y) {
           result.push(edge);
           this.union(parent, rank, x, y);
@@ -72,7 +64,6 @@ class Edge {
     }
   }
   
-  // Exemplo de uso
   const graph = new Graph(4);
   graph.addEdge(0, 1, 10);
   graph.addEdge(0, 2, 6);
@@ -86,8 +77,5 @@ class Edge {
   for (const edge of mst) {
     console.log(`Aresta ${edge.src} - ${edge.dest} com peso ${edge.weight}`);
   }
-  // Saída esperada:
-  // Aresta 2 - 3 com peso 4
-  // Aresta 0 - 3 com peso 5
-  // Aresta 0 - 1 com peso 10
+
   
